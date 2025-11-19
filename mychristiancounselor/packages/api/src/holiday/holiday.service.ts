7dc6f2b6cb707f3c2b8180ec870e1438d525d80b
Larry Tuck <friarmonk@gmail.com>
2025-11-19 00:24:08 -0900

    refactor(api): remove unused import in holiday service

--- mychristiancounselor/packages/api/src/holiday/holiday.service.ts
+++ mychristiancounselor/packages/api/src/holiday/holiday.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, NotFoundException, ForbiddenException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateHolidayDto } from './dto/create-holiday.dto';
 import { UpdateHolidayDto } from './dto/update-holiday.dto';
